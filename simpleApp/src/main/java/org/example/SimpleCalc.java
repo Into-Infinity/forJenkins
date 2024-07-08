7458f0e82f0d198cc49d317b9597b4d8d9c3139d
Vadim Fedulov <vadim_fedulov@senla.eu>
2024-07-08 14:21:10 +0300

    Add files for Simple app

---
+++ simpleApp/src/main/java/org/example/SimpleCalc.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class SimpleCalc {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+    }
+    
+}
---
+++ simpleApp/src/test/java/CalcUnitTests.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.*;
+import static org.example.SimpleCalc.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class CalcUnitTests {
+
+    @Test
+    @DisplayName("Add two numbers")
+    @Order(1)
+    void testCalcAdd()
+    {
+        Assertions.assertEquals( 4 , add(2, 2));
+        System.out.println("=======testCalcAdd EXECUTED=======");
+    }
+
+    @Test
+    @DisplayName("Difference between numbers")
+    @Order(2)
+    void testCalcSubtract()
+    {
+        Assertions.assertEquals( 0 , subtract(2, 2));
+        System.out.println("=======testCalcSubtract EXECUTED=======");
+    }
+
+    @Test
+    @DisplayName("Multiply two numbers")
+    @Order(3)
+    void testCalcMultiply()
+    {
+        Assertions.assertEquals( 4 , multiply(2, 2));
+        System.out.println("=======testCalcMultiply EXECUTED=======");
+    }
+
+    @Test
+    @DisplayName("Divide two numbers")
+    @Order(4)
+    void testCalcDivide()
+    {
+        Assertions.assertEquals( 0 , divide(2, 2));
+        System.out.println("=======testCalcDivide EXECUTED=======");
+    }
+
+}
